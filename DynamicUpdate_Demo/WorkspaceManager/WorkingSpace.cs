e3a8a41d55c4e4bd096ab0100392ee6d4977ff45
ngocpq <phungquangngoc@gmail.com>
2021-02-01 07:44:08 +0900

    WorkspaceManager, temp

--- DynamicUpdate_Demo/WorkspaceManager/WorkingSpace.cs
+++ DynamicUpdate_Demo/WorkspaceManager/WorkingSpace.cs
@@ -10,40 +10,17 @@ namespace WorkspaceManager
 {
     public class WorkingSpace
     {
-        static WorkingSpace _Instance;
-        
-        public static WorkingSpace Instance
-        {
-            get
-            {
-                if (_Instance == null)
-                    _Instance = new WorkingSpace();
-                return _Instance;
-            }
-        }
-
-        internal static void LoadWorkspace(string workspaceFile)
-        {
-            //TODO: load workspace from file
-            MessageBox.Show("Read workspace file");            
-            _Instance = new WorkingSpace(workspaceFile);
-
-        }
-
-        public static void ApplicationExit(object sender, EventArgs e)
-        {
-            MessageBox.Show("Application exiting");
-            if (Instance == null)
-                return;
-            Instance.SaveWorkspace();
-        }
 
         List<IWindowManager> OpeningWindows = new List<IWindowManager>();
+
+        Dictionary<string, AppDomain> ActiveAppDomains = new Dictionary<string, AppDomain>();
+
+        Dictionary<string, List<AppDomain>> Assembly2AppDomains = new Dictionary<string, List<AppDomain>>();
 
         private void SaveWorkspace()
         {
             StringBuilder sb = new StringBuilder();
-            foreach(IWindowManager w in this.OpeningWindows)
+            foreach (IWindowManager w in this.OpeningWindows)
             {
                 sb.Append("Form: " + w.ToString() + Environment.NewLine);
             }
@@ -57,7 +34,8 @@ namespace WorkspaceManager
 
         WorkspaceManagerConfig _Config;
 
-        public WorkingSpace(WorkspaceManagerConfig config) {
+        public WorkingSpace(WorkspaceManagerConfig config)
+        {
             this._Config = config;
         }
 
@@ -67,7 +45,80 @@ namespace WorkspaceManager
 
         protected WorkingSpace() { }
 
-        public static WorkingSpace GetCurrentWorkspace() {
+        public AppDomain CreateAppDomain(string name)
+        {
+            AppDomain appDomain;
+            if (this.ActiveAppDomains.ContainsKey(name))
+                appDomain = this.ActiveAppDomains[name];
+            else
+            {
+                appDomain = AppDomain.CreateDomain(name);
+                this.ActiveAppDomains[name] = appDomain;
+                appDomain.DomainUnload += AppDomain_DomainUnload; ;
+                appDomain.AssemblyLoad += AppDomain_AssemblyLoad;
+            }
+            return appDomain;
+        }
+
+        private void AppDomain_AssemblyLoad(object sender, AssemblyLoadEventArgs args)
+        {
+            AppDomain appDomain = sender as AppDomain;
+            if (appDomain == null)
+                return;
+
+            string asmLocation = args.LoadedAssembly.Location;
+            List<AppDomain> appDomains;
+            if (this.Assembly2AppDomains.ContainsKey(asmLocation))
+            {
+                appDomains = Assembly2AppDomains[asmLocation];
+            }
+            else
+            {
+                appDomains = new List<AppDomain>();
+                Assembly2AppDomains[asmLocation] = appDomains;
+            }
+            if (!appDomains.Contains(appDomain))
+                appDomains.Add(appDomain);
+        }
+
+        private void AppDomain_DomainUnload(object sender, EventArgs e)
+        {
+            //TODO: remove unloaded appDomain from the active list
+
+        }
+
+        #region Static members
+
+        static WorkingSpace _Instance;
+
+        public static WorkingSpace Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                    _Instance = new WorkingSpace();
+                return _Instance;
+            }
+        }
+
+        internal static void LoadWorkspace(string workspaceFile)
+        {
+            //TODO: load workspace from file
+            MessageBox.Show("Read workspace file");
+            _Instance = new WorkingSpace(workspaceFile);
+
+        }
+
+        public static void ApplicationExit(object sender, EventArgs e)
+        {
+            MessageBox.Show("Application exiting");
+            if (Instance == null)
+                return;
+            Instance.SaveWorkspace();
+        }
+
+        public static WorkingSpace GetCurrentWorkspace()
+        {
             return Instance;
         }
 
@@ -95,6 +146,10 @@ namespace WorkspaceManager
 
         }
 
+
+        #endregion
+
+
         public void RemoveOpeningWindow(IWindowManager window)
         {
             if (!this.OpeningWindows.Contains(window))
@@ -109,6 +164,6 @@ namespace WorkspaceManager
             this.OpeningWindows.Add(window);
         }
 
-        
+
     }
 }
